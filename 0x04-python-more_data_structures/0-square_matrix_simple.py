5e2810b264f05500095b1899e174a3cdf4065574
root <root@DESKTOP-VRHDERK>
2023-10-11 23:11:29 +0300

    si

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+matrix1 = [1, 2, 3]
+print(matrix1)
+new_matrix1 = list(map(lambda x: x ** 2 , matrix1))
+print(new_matrix1)
+matrix2 = [4, 5, 6]
+print(matrix2)
+new_matrix2 = list(map(lambda x: x ** 2 , matrix2))
+print(new_matrix2)
+matrix3 = [7, 8, 9]
+print(matrix3)
+new_matrix3 = list(map(lambda x: x ** 2 , matrix3))
+print(new_matrix3)
