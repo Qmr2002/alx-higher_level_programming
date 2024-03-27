3b2ef3dce2d2f7711c2a80da12f096fcdd472d0d
Hima <asondos867@gmail.com>
2024-03-27 03:15:11 +0200

    update js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,22 @@
-#!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
-      }
+const apiUrl = process.argv[2];
+
+request(apiUrl, function(error, response, body) {
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        const films = JSON.parse(body).results;
+        let wedgeMoviesCount = 0;
+        const wedgeCharacterId = '18';
+
+        films.forEach(film => {
+            const characters = film.characters;
+            if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeCharacterId}/`)) {
+                wedgeMoviesCount++;
+            }
+        });
+
+        console.log(wedgeMoviesCount);
     }
-    console.log(counter);
-  }
 });
